fd32352f43ddac67a470555f56f7b8f5c41811e2
wanglongsxr <xl432415>
2020-05-11 22:24:48 +0800

    修改readme，修改数据库sql

--- README.md
+++ README.md
@@ -1,22 +1,55 @@
 # springboot整合springsecurity 连载中...
+
 ## 项目介绍
+
 springboot整合springsecurity 的练手项目     
 项目管理工具为gradle
+
 ## 软件架构
+
 * springboot
 * springsecurity
 * oauth2
 * jwt
 * springcloud
 * gradle
+
 ## 使用说明
+
 * **boot_security_demo**    
   springboot与springsecurity整合的入门程序，即表单登录，普通授权。内容包括用户认证功能，自定义鉴权成功页面，自定义鉴权失败页面，普通授权功能，会话管理功能等...
+
 * **boot_security_remberme-code**    
-  springboot与springsecurity整合的"记住我"功能和"验证码"功能，基于boot_security_demo的项目拓展。    
-  此处的remember me功能是自定义数据库名，可能跟网上大多数文章实现不太一样      
+  springboot与springsecurity整合的"记住我"功能和"验证码"功能，基于boot_security_demo的项目拓展。 
+  此处的remember me功能是自定义数据库名.  
   验证码功能采用kaptcha插件
+
+* **boot_security_permission**  
+
+  springboot与springsecurity整合的动态权限认证功能，基于boot_security_demo的项目拓展。
+
+## 演示说明
+
+**演示过程**
+
+1. 登录前无法访问 /login-success
+
+2. 登录访问 http://localhost:8080/login（注意：各个工程分别对应各个的端口号）
+
+3. 账号密码 zhangsan/123    lisi/123
+
+4. 校验权限时，url为：http://localhost:8080/r/r1;    http://localhost:8080/r/r2;
+
+   zhangsan拥有p1和p2权限，李四拥有p1权限；
+
+   p1权限对应r1资源，p2权限对应r2资源；
+
+   无权限时会爆出403错误，forbidden
+
+5. 一旦点“退出登录”，session失效，remrember me功能则失效
+
 ## 解答与困惑
+
 * CSDN博客：<a>https://blog.csdn.net/zz18435842675</a>    
-emmm...实在搞不懂的话，qq:2647351651    
-如果您觉得写的还不错或者对您有帮助，请不要吝啬你的小星星（star一下），它将是我的最大支持
+  emmm...实在搞不懂的话，qq:2647351651    
+  如果您觉得写的还不错或者对您有帮助，请不要吝啬你的小星星（star一下），它将是我的最大支持
--- 数据库/boot_security.sql
+++ 数据库/boot_security.sql
@@ -11,20 +11,21 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 23/03/2020 18:18:30
+ Date: 11/05/2020 22:05:22
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
 -- ----------------------------
 -- Table structure for t_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `t_permission`;
 CREATE TABLE `t_permission`  (
-  `ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
   `CODE` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限标识符',
-  `DESCRIPTION` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `URL` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `DESCRIPTION` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `URL` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '路径',
   PRIMARY KEY (`ID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -47,32 +48,38 @@ CREATE TABLE `t_remember_token`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of t_remember_token
+-- ----------------------------
+INSERT INTO `t_remember_token` VALUES ('4aoHDXT63XGUh30I3jE8Ag==', 'zhangsan', 'Q0Q4zc6pRIHIcLr+duxn/g==', '2020-05-09 13:37:27');
+
+-- ----------------------------
 -- Table structure for t_role
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role`;
 CREATE TABLE `t_role`  (
-  `ID` int(11) NOT NULL,
-  `ROLE_NAME` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `DESCRIPTION` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `CREATE_TIME` datetime(0) NULL DEFAULT NULL,
-  `UPDATE_TIME` datetime(0) NULL DEFAULT NULL,
-  `STATUS` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ID` int(11) NOT NULL COMMENT '主键ID',
+  `ROLE_NAME` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+  `DESCRIPTION` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `CREATE_TIME` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `UPDATE_TIME` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `STATUS` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '状态',
   PRIMARY KEY (`ID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_role
 -- ----------------------------
-INSERT INTO `t_role` VALUES (1, '管理员', NULL, NULL, NULL, '');
+INSERT INTO `t_role` VALUES (1, 'admin', '管理员', '2020-05-09 10:10:55', '2020-05-09 10:10:58', '1');
+INSERT INTO `t_role` VALUES (2, 'test', '测试用户', '2020-05-09 10:11:41', '2020-05-09 10:11:43', '1');
 
 -- ----------------------------
 -- Table structure for t_role_permission
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role_permission`;
 CREATE TABLE `t_role_permission`  (
-  `ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `ROLE_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `PERMISSION_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
+  `ROLE_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色ID',
+  `PERMISSION_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '权限ID',
   PRIMARY KEY (`ID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -80,15 +87,17 @@ CREATE TABLE `t_role_permission`  (
 -- Records of t_role_permission
 -- ----------------------------
 INSERT INTO `t_role_permission` VALUES ('1', '1', '1');
+INSERT INTO `t_role_permission` VALUES ('2', '2', '2');
+INSERT INTO `t_role_permission` VALUES ('3', '1', '2');
 
 -- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user`  (
-  `ID` bigint(20) NOT NULL,
-  `USERNAME` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `PASSWORD` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ID` bigint(20) NOT NULL COMMENT '主键ID',
+  `USERNAME` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `PASSWORD` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
   `FULLNAME` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户姓名',
   `MOBILE` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
   PRIMARY KEY (`ID`) USING BTREE
@@ -98,16 +107,17 @@ CREATE TABLE `t_user`  (
 -- Records of t_user
 -- ----------------------------
 INSERT INTO `t_user` VALUES (1, 'zhangsan', '$2a$10$1b5mIkehqv5c4KRrX9bUj.A4Y2hug3IGCnMCL5i4RpQrYV12xNKye', '张三', '11111111111');
+INSERT INTO `t_user` VALUES (2, 'lisi', '$2a$10$1b5mIkehqv5c4KRrX9bUj.A4Y2hug3IGCnMCL5i4RpQrYV12xNKye', '李四', '22222222222');
 
 -- ----------------------------
 -- Table structure for t_user_role
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user_role`;
 CREATE TABLE `t_user_role`  (
-  `ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `USER_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `ROLE_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `CREATE_TIME` datetime(0) NULL DEFAULT NULL,
+  `ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '主键ID',
+  `USER_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户ID',
+  `ROLE_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '角色ID',
+  `CREATE_TIME` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `CREATOR` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
@@ -115,6 +125,7 @@ CREATE TABLE `t_user_role`  (
 -- ----------------------------
 -- Records of t_user_role
 -- ----------------------------
-INSERT INTO `t_user_role` VALUES ('1', '1', '1', NULL, NULL);
+INSERT INTO `t_user_role` VALUES ('1', '1', '1', '2020-05-09 10:12:11', NULL);
+INSERT INTO `t_user_role` VALUES ('2', '2', '2', '2020-05-09 10:12:25', NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
